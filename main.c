4cc6c62111dd5400969f4d523ef5844839b52e36
ethanG <ethangunzb@gmail.com>
2024-09-26 22:23:04 +0200

    working on place_token function

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 
 char** init_board(int rows, int columns);
 void print_board(char** board, int rows, int columns);
+char** place_token(char** board, int rows, int columns, char player);
 
 
 char** init_board(int rows, int columns){
@@ -26,6 +27,36 @@ void print_board(char** board, int rows, int columns){
     }
 }
 
+char** place_token(char** board, int rows, int columns, char player){
+    int flag = 0;
+    int token;
+    do{
+        printf("%c select where you want to place your token: ", player);
+        scanf("%d",&token);
+        if(token < 1 || token > columns){
+            flag = 1;
+            printf("token out of grid");
+            fflush(stdin);
+        }
+        for(int i = rows; i >= 0; i--) {
+            if(board[i][token-1] == 0 ){
+                board[i][token-1] = player;
+                printf("test : %c",board[i][token-1]);
+                break;
+            }
+            if(board[0][token-1] != 0 ){
+                printf("token out of grid");
+                fflush(stdin);
+                flag = 1;
+            }
+        }
+
+    }while(flag != 0);
+
+
+
+}
+
 int main( )
 {
     int rows = 6;
@@ -33,6 +64,9 @@ int main( )
 
     char** board = init_board(rows, columns);
     print_board(board, rows, columns);
+    place_token(board, rows, columns, 'x');
+    print_board(board, rows, columns);
+
     return 666;
 }
 
