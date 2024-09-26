973b929aa064e0de249be41b9e77af7a72efd7b0
ethanG <ethangunzb@gmail.com>
2024-09-27 01:38:33 +0200

    verif rows work

--- main.c
+++ main.c
@@ -4,6 +4,8 @@
 char** init_board(int rows, int columns);
 void print_board(char** board, int rows, int columns);
 char** place_token(char** board, int rows, int columns, char player);
+char switch_player(char c);
+int verif_columns(char **board, int rows, int columns);
 
 
 char** init_board(int rows, int columns){
@@ -14,7 +16,6 @@ char** init_board(int rows, int columns){
     }
     return board;
 };
-
 
 void print_board(char** board, int rows, int columns){
     for(int i = 0; i < rows; i++){
@@ -59,20 +60,78 @@ char** place_token(char** board, int rows, int columns, char player){
 
 }
 
+char switch_player(char c) {
+    return c == 'x' ? 'y' : 'x';
+}
+
+
+int verif_columns(char **board, int rows, int columns) {
+    int puissance = 4;
+    int verif = 0;
+
+    for (int x = 0; x < rows; x++) {
+        for (int y = 0; (y + puissance-1) < columns; y++) {
+            verif = 0;
+            for (int c = 1; c < puissance || c < columns; c++) {
+                if (board[x][y] == board[x][y + c] && board[x][y] != 0)
+                    verif++;
+                else
+                    verif = 0;
+                if (verif == puissance - 1) {
+                    return 1;
+                }
+            }
+        }
+    }
+    return 0;
+}
+
+
+
+int verif_win(char **board, int rows, int columns) {
+    int win = 0;
+
+    win = verif_columns(board, rows, columns);
+
+    return win;
+
+}
+
 int main( )
 {
     int rows = 6;
     int columns = 7;
+    char joueur = 'x';
+    int isWin = 0;
 
     char** board = init_board(rows, columns);
-    while(1){
+    while(isWin == 0) {
         print_board(board, rows, columns);
-        board = place_token(board, rows, columns, 'x');
-        print_board(board, rows, columns);
-        board = place_token(board, rows, columns, 'Y');
+        board = place_token(board, rows, columns, joueur);
+        isWin = verif_win(board, rows, columns);
+        if (isWin == 1) {
+            print_board(board, rows, columns);
+            printf("%c gagne !\n", joueur);
+            break;
+        }
+        joueur = switch_player(joueur);
     }
+
+
 
 
     return 666;
 }
 
+
+
+/*
+ *     for (int c = 1; c < puissance || c < columns; c++) {
+                if (board[x][y] == board[x][y + c] && board[x][y] != 0)
+                    verif++;
+                if (verif == puissance) {
+                    printf("ttttttt\n");
+                    return 1;
+                }
+            }
+ */
